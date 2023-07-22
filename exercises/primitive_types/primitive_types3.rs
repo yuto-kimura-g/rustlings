3b19c5f5a353c7c04198e3fe2bb32b44a72f11cf
yuto-kimura-g <75498217+yuto-kimura-g@users.noreply.github.com>
2023-07-23 01:13:15 +0900

    update: enums, move_semantics, primitive_types, strings, structs, vecs

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -5,10 +5,12 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let a = ???
+    let a = vec![1; 100];
+    // let mut a = Vec::<i32>::new();
+    // for i in 1..1000 {
+    //     a.push(i);
+    // }
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,35 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+/*
+"文字列" -> &str
+String::from("文字列") -> String
+"文字列".to_string() -> String
+String::from("文字列").as_str() -> &str
+*/
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    input.trim().to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    // ver:1
+    format!("{} world!", input)
+    // ver:2
+    // let mut s = String::from(input);
+    // s += " world!";
+    // s
+    // ver:3
+    // let mut s = String::from(input);
+    // s.push_str(" world!");
+    // s
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    input.replace("cars", "balloons")
 }
 
 #[cfg(test)]
@@ -39,7 +53,13 @@ mod tests {
 
     #[test]
     fn replace_a_string() {
-        assert_eq!(replace_me("I think cars are cool"), "I think balloons are cool");
-        assert_eq!(replace_me("I love to look at cars"), "I love to look at balloons");
+        assert_eq!(
+            replace_me("I think cars are cool"),
+            "I think balloons are cool"
+        );
+        assert_eq!(
+            replace_me("I love to look at cars"),
+            "I love to look at balloons"
+        );
     }
 }
